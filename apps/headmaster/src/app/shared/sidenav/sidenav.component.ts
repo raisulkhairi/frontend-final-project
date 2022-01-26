42e7066949a3ce9f7964c09bc644f0b30f555814
asmon <oscarsolideo@gmail.com>
2022-01-26 20:02:39 +0700

    rev3

--- apps/headmaster/src/app/shared/sidenav/sidenav.component.ts
+++ apps/headmaster/src/app/shared/sidenav/sidenav.component.ts
@@ -15,11 +15,54 @@ export class SidenavComponent implements OnInit {
   showStudent = false;
   showParent = false;
   showClass = false;
+  count1 = 0;
+  count2 = 0;
+  count3 = 0;
 
   constructor(private authorizationService: AuthorizationService) {}
 
   ngOnInit(): void {}
   logoutUser() {
     this.authorizationService.logout();
+  }
+  openTeacher() {
+    this.count1++;
+    if (this.count1 % 2 == 1) {
+      this.showTeacher = true;
+      this.showStudent = false;
+      this.showParent = false;
+    } else {
+      this.showTeacher = false;
+      this.showStudent = false;
+      this.showParent = false;
+    }
+    this.count1 %= 2;
+  }
+  openStudent() {
+    this.count2++;
+    if (this.count2 % 2 == 1) {
+      this.showTeacher = false;
+      this.showStudent = true;
+      this.showParent = false;
+    } else {
+      this.showTeacher = false;
+      this.showStudent = false;
+      this.showParent = false;
+    }
+    this.count2 %= 2;
+  }
+  openParent() {
+    this.count3++;
+    if (this.count3 % 2 == 1) {
+      this.showTeacher = false;
+      this.showStudent = false;
+      this.showParent = true;
+    } else {
+      this.showTeacher = false;
+      this.showStudent = false;
+      this.showParent = false;
+    }
+
+    this.count3 %= 2;
   }
 }
